935d54ef25f0e9d18f309fb2a102341765e43362
Tommaso Venza <tommasovenza@gmail.com>
2020-07-01 13:37:12 +0200

    all'evento click sul button stampo a schermo i 4 valori richiesti all'APi

---
+++ js/script.js
@@ -0,0 +1,74 @@
+// Chiave API (v3 auth)
+// e12dca5dd96a7799461651a590256acb
+
+// Esempio Richiesta API
+// https://api.themoviedb.org/3/movie/550?api_key=e12dca5dd96a7799461651a590256acb
+
+$(document).ready(function() {
+
+  // evento click
+  $(document).on('click', '#btn-ricerca', function() {
+
+    var valoreInput = $('#ricerca-film').val();
+
+    stampaFilm(valoreInput);
+
+  });
+
+}); // end document ready
+
+function stampaFilm(arrayRicerca) {
+
+  $.ajax( {
+
+    url:"https://api.themoviedb.org/3/search/movie",
+    method:"GET",
+
+    data: {
+      api_key: 'e12dca5dd96a7799461651a590256acb',
+      query: arrayRicerca,
+      language: 'it-IT'
+    },
+
+    success: function(data) {
+       
+      var source = $('#film-template').html();
+      var template = Handlebars.compile(source);
+
+      var arrayRicerca = data.results;
+      
+      for (var i = 0; i < arrayRicerca.length; i++) {
+
+        var filmSingolo = arrayRicerca[i];
+
+        var titolo = filmSingolo.title;
+        var titoloOriginale = filmSingolo.original_title;
+        var linguaOriginale = filmSingolo.original_language;
+        var votoMedio = filmSingolo.vote_average;
+
+        // console.log(titolo);
+        // console.log(titoloOriginale);
+        // console.log(linguaOriginale);
+        // console.log(votoMedio);
+
+        var context = {
+          title: titolo,
+          original_language: linguaOriginale,
+          original_title: titoloOriginale,
+          vote_average: votoMedio
+        };
+  
+        var html = template(context);
+  
+        $('#stampa-ul').append(html);
+      }
+
+    },
+
+    error: function() {
+      alert('qualcosa non va');
+    }
+
+
+  });
+}
